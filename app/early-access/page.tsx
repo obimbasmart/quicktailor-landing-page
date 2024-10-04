0954966583f012955d6e6fd95110cc1b70fa8b9b
memmalino <memmalino#gmail.com>
2024-10-04 20:27:11 +0100

    forms ready

--- app/early-access/page.tsx
+++ app/early-access/page.tsx
@@ -1,7 +1,58 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import { useState } from 'react';
 
 function EarlyAccess() {
+    const [formData, setFormData] = useState({
+        last_name: '',
+        first_name: '',
+        email: '',
+        agree: false 
+    });
+
+    const handleChange = (e: any) => {
+        const { name, type, value, checked } = e.target;
+        setFormData({
+            ...formData,
+            [name]: type === 'checkbox' ? checked : value
+        });
+        console.log(formData);
+    };
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+
+        const { first_name, email, last_name, agree } = formData;
+
+    
+        const response = await fetch('/api/waitList', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                message: `Name: ${first_name + ' ' + last_name} \nEmail: ${email}`,
+                to: 'JCkfxhxt9S45cgIma7sE3N', // recipient's WhatsApp number
+            }),
+        });
+
+        const result = await response.json();
+
+        if (result.success) {
+            console.log('Message sent:', result.messageResponse);
+
+          
+            setFormData({
+                last_name: '',
+                first_name: '',
+                email: '',
+                agree: false 
+            });
+        } else {
+            console.error('Error sending message:', result.error);
+        }
+    };
+
     return (
         <section>
             <div>
@@ -30,41 +81,47 @@ function EarlyAccess() {
                     be the first to access atelyo.
                 </p>
 
-                <form className="max-w-sm mx-auto">
+                <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
                     <div className="flex flex-row gap-x-3 mb-3">
                         <div className="">
                             <label
-                                htmlFor="email"
+                                htmlFor="first_name"
                                 className="text-start block mb-1 text-sm font-medium text-gray-1000 dark:text-white"
                             >
                                 First Name
                             </label>
                             <input
-                                type="first-name"
+                                type="text"
                                 id="text"
+                                name="first_name"
+                                value={formData.first_name}
                                 className="border-gray-500 border-[1px]  text-gray-1000 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 required
+                                onChange={handleChange}
                             />
                         </div>
                         <div className="">
                             <label
-                                htmlFor="email"
+                                htmlFor="last_name"
                                 className="text-start block mb-1 text-sm font-medium text-gray-1000 dark:text-white"
                             >
                                 Last Name
                             </label>
                             <input
-                                type="first-name"
+                                type="text"
                                 id="text"
+                                name="last_name"
+                                value={formData.last_name}
                                 className="border-gray-500 border-[1px]  text-gray-1000 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 required
+                                onChange={handleChange}
                             />
                         </div>
                     </div>
 
                     <div className="mb-5">
                         <label
-                            htmlFor="password"
+                            htmlFor="email"
                             className="text-start block mb-1 text-sm  text-gray-1000 dark:text-white"
                         >
                             Email
@@ -72,23 +129,21 @@ function EarlyAccess() {
                         <input
                             type="email"
                             id="email"
+                            name="email"
+                            value={formData.email}
                             className="border border-gray-500 text-gray-1000 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required
+                            onChange={handleChange}
                         />
                     </div>
 
-                    <button
-                        type="submit"
-                        className="mb-5 text-white bg-primary hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-hmedium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    >
-                        Submit
-                    </button>
-
-                    <div className="flex items-start">
+                    <div className="flex items-start mb-4">
                         <input
                             id="link-checkbox"
                             type="checkbox"
-                            value=""
+                            name="agree"
+                            checked={formData.agree}
+                            onChange={handleChange}
                             className="w-5 h-5 text-teal-600  border-gray-1000 rounded focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                         />
                         <label
@@ -99,7 +154,14 @@ function EarlyAccess() {
                             and Terms & Conditions. We won't share your data.
                             View our Terms & Conditions and Privacy Policy.
                         </label>
-                    </div> 
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="mb-5 text-white bg-primary hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-hmedium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        Submit
+                    </button>
                 </form>
             </div>
         </section>
